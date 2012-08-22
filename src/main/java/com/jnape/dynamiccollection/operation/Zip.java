7a6dc582623b534d3d4a1e656379e26701c83857
John Napier <jnape09@gmail.com>
2012-08-21 20:39:39 -0500

    Adding Zip operation to zip lists together

---
+++ src/main/java/com/jnape/dynamiccollection/operation/Zip.java
@@ -0,0 +1,21 @@
+package com.jnape.dynamiccollection.operation;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Zip {
+
+    public static <Element> List<List<Element>> zip(List<Element> heads, List<Element>... tails) {
+        List<List<Element>> zipped = new ArrayList<List<Element>>();
+
+        for (int i = 0; i < heads.size(); i++) {
+            List<Element> entry = new ArrayList<Element>();
+            entry.add(heads.get(i));
+            for (List<Element> tail : tails)
+                entry.add(tail.size() > i ? tail.get(i) : null);
+            zipped.add(entry);
+        }
+
+        return zipped;
+    }
+}
---
+++ src/test/java/com/jnape/dynamiccollection/operation/ZipTest.java
@@ -0,0 +1,59 @@
+package com.jnape.dynamiccollection.operation;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static com.jnape.dynamiccollection.factory.DynamicListFactory.list;
+import static java.util.Arrays.asList;
+import static org.junit.Assert.assertEquals;
+
+@SuppressWarnings("unchecked")
+public class ZipTest {
+
+    @Test
+    public void shouldZipJustOneList() {
+        List<Integer> oneThroughFive = list(1, 2, 3, 4, 5);
+
+        assertEquals(list(
+                list(1),
+                list(2),
+                list(3),
+                list(4),
+                list(5)
+        ), Zip.zip(oneThroughFive));
+    }
+
+    @Test
+    public void shouldZipTwoListsOfSameSize() {
+        List<Integer> odds = asList(1, 3);
+        List<Integer> evens = asList(2, 4);
+
+        assertEquals(list(
+                list(1, 2),
+                list(3, 4)
+        ), Zip.zip(odds, evens));
+    }
+
+    @Test
+    public void shouldZipTwoListsOfDifferentSizeUsingNullForPaddingIfNoPaddingProvided() {
+        List<Character> abc = asList('a', 'b', 'c');
+        List<Character> oneTwo = asList('1', '2');
+
+        assertEquals(list(list('a', '1'), list('b', '2'), list('c', null)), Zip.zip(abc, oneTwo));
+    }
+
+    @Test
+    public void shouldZipThreeOrMoreLists() {
+        List<Object> odds = Arrays.<Object>asList(1, 3, 5);
+        List<Object> evens = Arrays.<Object>asList(2, 4, 6);
+        List<Object> abc = Arrays.<Object>asList('a', 'b', 'c');
+
+        assertEquals(list(
+                list(1, 2, 'a'),
+                list(3, 4, 'b'),
+                list(5, 6, 'c')
+        ), Zip.zip(odds, evens, abc));
+    }
+}
